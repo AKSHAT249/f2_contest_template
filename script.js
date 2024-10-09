f0482ec42091dca5b3c9043a51cf44e0b48cbca9
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-09 16:30:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
-];
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    //Write your code here , just console.log
+    arr.map((currEle) => {
+        if(currEle.profession === "developer"){
+            console.log(currEle);
+        }
 
-function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
-}
+    })
+  }
+  
+  function PrintDeveloperbyForEach() {
+    //Write your code here , just console.log
+    arr.forEach((data, index) => {
+        if(data.profession === "developer"){
+            console.log(data);
+        }
 
-function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
-}
+    })
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+    let newData =  {id:4, name:"susan", age:"20", profession:"intern"}
+    arr.push(newData);
+    console.log(arr);
+  }
+  
+  function removeAdmin() {
+    //Write your code here, just console.log
+    let data = arr.filter( (item) => {
+        
+            if(item.profession!="admin"){
+                return true;
+            }
+        
 
-function addData() {
-  //Write your code here, just console.log
-}
+    })
+    console.log(data);
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    let newEntry = [
+        { id: 4, name: "peter", age: "28", profession: "analyst" },
+        { id: 5, name: "maria", age: "29", profession: "AI engineer" },
+        { id: 6, name: "jack", age: "12", profession: "admin" },
+    ]
 
-function removeAdmin() {
-  //Write your code here, just console.log
-}
+    arr = arr.concat(newEntry);
+    console.log(arr);
 
-function concatenateArray() {
-  //Write your code here, just console.log
-}
+  }
